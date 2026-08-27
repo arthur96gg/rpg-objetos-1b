501625d852a2b9cb756dce150140335828a53d33
arthur96gg <arthur.9166@colegioprogresso.g12.br>
2026-08-27 07:36:55 -0300

    classe

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
-class Personagem{
-  constructor(nome,vida,ataque){
-    this.nome = nome,
-    this.vida = vida,
-    this.ataque = ataque
-  }
- causaDano(personagem){
-  personagem.recebeDano(this.ataque)
- }
- recebeDano(quntidade){
-  this.vida = this.vida - quntidade
-  if(this.vida <=0)
-    this.vida ="morreu"
- }
+class Personagem {
+    constructor(nome, vida, ataque) {
+        this.nome = nome,
+            this.vida = vida,
+            this.ataque = ataque
+    }
+    causaDano(personagem) {
+        personagem.recebeDano(this.ataque)
+    }
+    recebeDano(quntidade) {
+        this.vida = this.vida - quntidade
+        if (this.vida <= 0)
+            this.vida = "morreu"
+    }
+    estaVivo() {
+        return this.vida > 0
+    }
+    mostraStatus() {
+        console.log(this.nome + "| vida" + this.vida)
+    }
+
 }
 
-const druida = new Personagem("obin", 150 ,15)
-const guerreiro = new Personagem("thorfin",60,20)
-const arqueiro = new Personagem("legolas",50,40)
-const mago = new Personagem("Gandalf",40,70)
-const assasino = new Personagem("mortis",30.60)
-const clerigo = new Personagem("Ezra", 40,20)
-const monge =  new Personagem("tatsu",60,30)
+const druida = new Personagem("obin", 150, 15)
+const guerreiro = new Personagem("thorfin", 60, 20)
+const arqueiro = new Personagem("legolas", 50, 40)
+const mago = new Personagem("Gandalf", 40, 35)
+const assasino = new Personagem("mortis", 30, 50)
+const clerigo = new Personagem("Ezra", 40, 20)
+const monge = new Personagem("tatsu", 60, 30)
 
-console.log(monge)
+mago.causaDano(assasino)
+assasino.mostraStatus()
+console.log(assasino.estaVivo())
